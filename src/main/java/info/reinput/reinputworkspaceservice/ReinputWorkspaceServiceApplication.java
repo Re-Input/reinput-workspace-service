01dfca239f3dc961a0a12d9bfed784af5d14b9ad
Jhsysng <ysng1118@naver.com>
2025-01-13 12:31:01 +0900

    init spring

---
+++ src/main/java/info/reinput/reinputworkspaceservice/ReinputWorkspaceServiceApplication.java
@@ -0,0 +1,13 @@
+package info.reinput.reinputworkspaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinputWorkspaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReinputWorkspaceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/reinput/reinputworkspaceservice/ReinputWorkspaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package info.reinput.reinputworkspaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinputWorkspaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
