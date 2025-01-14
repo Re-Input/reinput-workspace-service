d86588e9bfa9ef272462c3510ac4e8825eac602b
MinhoJJang <ahchjang@naver.com>
2025-01-14 17:06:45 +0900

    Enable Eureka client and migrate configuration to YAML format. Removed application.properties and added application.yml with server and Eureka client settings.

--- src/main/java/info/reinput/reinputworkspaceservice/ReinputWorkspaceServiceApplication.java
+++ src/main/java/info/reinput/reinputworkspaceservice/ReinputWorkspaceServiceApplication.java
@@ -2,8 +2,10 @@ package info.reinput.reinputworkspaceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReinputWorkspaceServiceApplication {
 
     public static void main(String[] args) {
