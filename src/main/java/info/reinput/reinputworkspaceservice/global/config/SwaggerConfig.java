dc68bad8219685d5f4e315f9a7706eb92b31fcb0
Jhsysng <ysng1118@naver.com>
2025-01-25 00:05:57 +0900

    RE-164 feat: folder swagger userId 파라미터 제거설정2

--- src/main/java/info/reinput/reinputworkspaceservice/global/config/SwaggerConfig.java
+++ src/main/java/info/reinput/reinputworkspaceservice/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package info.reinput.reinputworkspaceservice.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -29,9 +30,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .servers(createServers())
                 .info(createApiInfo());
+
+        // X-User-Id 헤더를 전역적으로 제거
+        openAPI.getPaths().forEach((path, pathItem) ->
+                pathItem.readOperations().forEach(operation -> {
+                    List<Parameter> parameters = operation.getParameters();
+                    if (parameters != null) {
+                        parameters.removeIf(param -> "X-User-Id".equalsIgnoreCase(param.getName()));
+                    }
+                })
+        );
+
+        return openAPI;
     }
 
     private List<Server> createServers() {
@@ -47,10 +60,6 @@ public class SwaggerConfig {
                 .version("v0.0.1")
                 .description("""
                         Reinput Folder Service (workspace service)
-                        
-                        인증 플로우:
-                        1. API Gateway에서 `X-User-Id` 헤더를 자동으로 추가
-                        2. 클라이언트는 JWT를 Authorization 헤더로 제공하여 인증
                         """);
     }
 }
