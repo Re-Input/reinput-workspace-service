67fd2cce7e354fd676274b7ce7e3c0622b1ff898
Jhsysng <ysng1118@naver.com>
2025-01-29 21:34:27 +0900

    RE-179 chore: feign client의존성 추가

---
+++ src/main/java/info/reinput/reinputworkspaceservice/folder/infra/client/ContentClient.java
@@ -0,0 +1,22 @@
+package info.reinput.reinputworkspaceservice.folder.infra.client;
+
+import info.reinput.reinputworkspaceservice.folder.presentation.dto.res.ApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "content-service")
+public interface ContentClient {
+    @GetMapping("/insight/count/folder/{folderId}")
+    ApiResponse<Integer> countInsight(
+            @PathVariable final Long folderId,
+            @RequestHeader("X-User-Id") final Long memberId
+    );
+
+    @GetMapping("/insight/count/folders/{folderIds}")
+    ApiResponse<List<Integer>> countInsight(
+            @PathVariable final List<Long> folderIds,
+            @RequestHeader("X-User-Id") final Long memberId
+    );
+}
